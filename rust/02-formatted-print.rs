9f78dee9285f11138f08c8bfaafc908b84b1ba07
Utkarsh Sinha <sinha.utkarsh1990@gmail.com>
2014-07-19 01:44:19 +0530

    rust 3/49

---
+++ rust/02-formatted-print.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("January has");
+
+    println!("{} days and {} hours", 31i, 4i);
+
+    println!("{0}, this is {1}. {1} this is {0}", "Alice", "Bob");
+
+    println!("{subject} {verb} {predicate}", predicate="over the lazy dog", subject="The quick brown fox", verb="jumps");
+
+    println!("{} of {:t} people know binary, the other half don't", 1i, 2i);
+
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+}
